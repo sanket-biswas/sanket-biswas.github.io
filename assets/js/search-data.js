c8fe8dd133a71ce90be8d8c8e576d167c829dc69
sanket-biswas <sanket.ubc.cheme@gmail.com>
2025-12-17 13:38:22 +0000

    Deploying to gh-pages from @ sanket-biswas/sanket-biswas.github.io@5df2fb185d55d4571d663ac73a3f5f04fa23a3c0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,7 +508,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=ljef_ZAAAAAJ", "_blank");
         },
       },{
         id: 'social-cv',

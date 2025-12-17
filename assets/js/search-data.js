44068edf70f1e840c802fa9a453dab27f63e4b58
sanket-biswas <sanket.ubc.cheme@gmail.com>
2025-12-17 12:28:56 +0000

    Deploying to gh-pages from @ sanket-biswas/sanket-biswas.github.io@1fc214f2f8012dd8252a8fdb0e80a38000d9d10b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-talks",
-          title: "Talks",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/conferences/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -43,6 +36,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
+          },
+        },{id: "nav-talks",
+          title: "Talks",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/conferences/";
           },
         },{id: "nav-courses",
           title: "Courses",

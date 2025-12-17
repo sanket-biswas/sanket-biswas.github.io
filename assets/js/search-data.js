c652c9e78947263bf607a012f3d328e3a1a02d5a
sanket-biswas <sanket.ubc.cheme@gmail.com>
2025-12-17 12:16:00 +0000

    Deploying to gh-pages from @ sanket-biswas/sanket-biswas.github.io@efc7bd9725052f9900423bf740466844473f5820 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/courses/";
           },
+        },{id: "nav-notes-amp-resources",
+          title: "Notes &amp; Resources",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/resources/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",

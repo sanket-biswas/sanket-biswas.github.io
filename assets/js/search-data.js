1ae05d1047e7b5cff3d26a9170e8480591eac344
sanket-biswas <sanket.ubc.cheme@gmail.com>
2025-12-18 01:55:48 +0000

    Deploying to gh-pages from @ sanket-biswas/sanket-biswas.github.io@98caeea08473bef122ecab47f64e5f41e76026a7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,13 +72,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "nav-notes",
-          title: "Notes",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/resources/";
-          },
         },{id: "nav-blog",
           title: "Blog",
           description: "",

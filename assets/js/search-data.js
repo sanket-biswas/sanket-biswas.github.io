8ac591e9fd4a5125db037c96b6a22fe731dff7d1
sanket-biswas <sanket.ubc.cheme@gmail.com>
2025-12-18 01:55:50 +0000

    Deploying to gh-pages from @ sanket-biswas/sanket-biswas.github.io@2b39b5390cba5061ccb4396cd9611e92f46ee2f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,13 +72,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
